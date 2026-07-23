48211a45b6d731358f4d6aeaab84f8ee99bf33db
Alejandro Herrera <j.alejandroherrera@gmail.com>
2026-07-23 15:52:07 -0300

    Screen to add new member

--- Backend/src/routes/members.routes.ts
+++ Backend/src/routes/members.routes.ts
@@ -10,7 +10,7 @@ import { authPermission } from "../middlewares/authorizePermission.middleware";
 const router = Router();
 
 router.get("/", /*authPermission("member:read"),*/ getMembers);
-router.post("/", authPermission("member:create"), addMember);
+router.post("/", /*authPermission("member:create"),*/ addMember);
 router.put("/:id", updateMember);
 router.delete("/:id", authPermission("member:delete"), deleteMember);
 
